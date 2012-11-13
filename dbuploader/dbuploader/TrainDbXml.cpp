326314da572c67294c24798bb521b8358cef16fc
gazovic.tomas <gazovic.tomas@385ec17a-00dc-d1df-55a0-da4308e74ed9>
2012-11-13 18:40:55 +0000

    dbuploader: konvertovanie stringu z xml suboru (train vektor) na pole hodnotu typu double.

--- dbuploader/dbuploader/TrainDbXml.cpp
+++ dbuploader/dbuploader/TrainDbXml.cpp
@@ -1,11 +1,12 @@
 #include "stdafx.h"
 #include "TrainDbXml.h"
 #include "Person.h"
+#include "SplitString.h"
 
 using namespace std;
 using namespace pugi;
 
-TrainDbXml::TrainDbXml(string fileName, deque<Person> persones) : XmlParsing(fileName)
+TrainDbXml::TrainDbXml(string fileName, deque<Person>& persones) : XmlParsing(fileName)
 {
 	// najprv sa overi ci pocet osob sedi s poctom trenovacich vektorov
 	int trainVectors = this->xmlDocument.child("opencv_storage").child("trainPersonNumMat").child("cols").text().as_int();
@@ -22,11 +23,40 @@ TrainDbXml::TrainDbXml(string fileName, deque<Person> persones) : XmlParsing(fil
 	// vypisanie jednotlivych trenovacich vektorov k osobam ku ktorym patria podla definicie osob v dbuploader.xml
 	for (int i = 0; i < persones.size(); i++)
 	{
-		wcout << persones[i].Name << endl;
-		wcout << persones[i].OpencvMetrixId.c_str() << endl;
+		//wcout << persones[i].Name << endl;
+		//wcout << persones[i].OpencvMatrixId.c_str() << endl;
 
-		string rawTrainVector = this->xmlDocument.child("opencv_storage").child(persones[i].OpencvMetrixId.c_str()).child("data").text().as_string();
+		string rawTrainVector = this->xmlDocument.child("opencv_storage").child(persones[i].OpencvMatrixId.c_str()).child("data").text().as_string();
 
-		wcout << rawTrainVector.c_str() << endl;
+		//wcout << rawTrainVector.c_str() << endl;
+		SplitString str(rawTrainVector);
+		// split retazca trenovacieho vektora na retazce hodnot
+		vector<string> sValues = str.split(' ', 1);
+		
+		// kontrola ci sa nacital spravny pocet poloziek vektoru
+		// pocet poloziek trenovacieho vektora = rows * cols
+		int rows = this->xmlDocument.child("opencv_storage").child(persones[i].OpencvMatrixId.c_str()).child("rows").text().as_int();
+		if (rows <= 0)
+		{
+			this->ErrorMsg("[TrainDbXml::TrainDbXml(string fileName, deque<Person> persones)]: Error while parsing dbuploader.xml");
+		}
+
+		int cols = this->xmlDocument.child("opencv_storage").child(persones[i].OpencvMatrixId.c_str()).child("cols").text().as_int();
+		if (cols <= 0)
+		{
+			this->ErrorMsg("[TrainDbXml::TrainDbXml(string fileName, deque<Person> persones)]: Error while parsing dbuploader.xml");
+		}
+
+		if (sValues.size() != rows * cols)
+		{
+			this->ErrorMsg("[TrainDbXml::TrainDbXml(string fileName, deque<Person> persones)]: Error while parsing dbuploader.xml");
+		}	
+		
+		// prevod retazcov cisel na pole hodnot typu double
+
+		for(int j = 0; j < sValues.size(); j++)
+		{
+			persones[i].OpencvMatrix.push_back(strtod(sValues[j].c_str(), 0));
+		}
 	}
 }
--- dbuploader/dbuploader/stdafx.h
+++ dbuploader/dbuploader/stdafx.h
@@ -14,6 +14,7 @@
 #include <tchar.h>
 #include <iostream>
 #include <deque>
+#include <vector>
 
 
 
