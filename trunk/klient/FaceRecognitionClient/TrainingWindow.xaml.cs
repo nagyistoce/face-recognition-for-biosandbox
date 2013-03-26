a892a098f7fff8c56c1884bd3e1a32cd4facf14b
gazovic.tomas <gazovic.tomas@385ec17a-00dc-d1df-55a0-da4308e74ed9>
2013-03-26 17:26:48 +0000

    klient - spustanie biosandboxu z aplikacie

--- trunk/klient/FaceRecognitionClient/TrainingWindow.xaml.cs
+++ trunk/klient/FaceRecognitionClient/TrainingWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using FaceRecognitionClient.Threading;
+using System.Threading;
 
 namespace FaceRecognitionClient
 {
@@ -19,35 +21,60 @@ namespace FaceRecognitionClient
     /// </summary>
     public partial class TrainingWindow : Window
     {
+        private MainWindow _parent;
+        private BackgroundWorkerControl _bc;
+        private DispatcherTimer _dispatcherTimer;
+
         public TrainingWindow()
         {
             InitializeComponent();
-            
+
+            this.Top = 0;
+            this.Left = SystemParameters.PrimaryScreenWidth - this.Width;
+        }
+
+        public void Show(MainWindow parent)
+        {
+            _parent = parent;
+            this.Show();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            DispatcherTimer dispatcherTimer = new DispatcherTimer();
-            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            _dispatcherTimer = new DispatcherTimer();
+            _dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            _dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
             label2.Content = 0;
-            dispatcherTimer.Start();
+            
+            _dispatcherTimer.Start();
+
+            
+            _bc = new BackgroundWorkerControl(_parent.BackgroundWorkerCompleted, Environment.ExpandEnvironmentVariables("%BIOSANDBOX_HOME%"));
+            _bc.AsyncTrening();
         }
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
+            
             int i = Int32.Parse(label2.Content.ToString());
+            if(i==0)
+                Thread.Sleep(1000);
             i++;
             label2.Content = i;
 
-            if (i < 4)
+            if (i < 7)
                 label1.Content = "Pozeraj sa rovno";
-            else if (i < 7)
-                label1.Content = "Natoč sa doľava";
             else if (i < 11)
+                label1.Content = "Natoč sa doľava";
+            else if (i < 15)
                 label1.Content = "Natoč sa doprava";
             else
+            {
+                //_parent.textBox1.Text += i;
+                _bc.BeginTreningEnd();
+                _dispatcherTimer.Stop();
                 this.Close();
+            }
             
         }
     }
