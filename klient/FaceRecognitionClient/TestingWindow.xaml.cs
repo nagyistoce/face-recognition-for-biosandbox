5377a65285aa6ea4508ce97012656a8b5b442f14
gazovic.tomas <gazovic.tomas@385ec17a-00dc-d1df-55a0-da4308e74ed9>
2013-04-24 15:26:17 +0000

    klient - automaticke testovanie subjektu

--- klient/FaceRecognitionClient/TestingWindow.xaml.cs
+++ klient/FaceRecognitionClient/TestingWindow.xaml.cs
@@ -10,26 +10,21 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.Windows.Threading;
 using FaceRecognitionClient.Threading;
-using System.Threading;
 using System.ComponentModel;
 
 namespace FaceRecognitionClient
 {
     /// <summary>
-    /// Interaction logic for TrainingWindow.xaml
+    /// Interaction logic for TestingWindow.xaml
     /// </summary>
-    public partial class TrainingWindow : Window
+    public partial class TestingWindow : Window
     {
         private MainWindow _parent;
         private BackgroundWorkerControl _bc;
-        private DispatcherTimer _dispatcherTimer;
 
-        private string _personName;
-        private DateTime[] _times;
 
-        public TrainingWindow()
+        public TestingWindow()
         {
             InitializeComponent();
 
@@ -40,66 +35,19 @@ namespace FaceRecognitionClient
         public void Show(MainWindow parent, string personName)
         {
             _parent = parent;
-            _personName = personName;
-            _times = new DateTime[3];
             this.Show();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            _dispatcherTimer = new DispatcherTimer();
-            _dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-            _dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
-            label2.Content = 0;
-            
-            _dispatcherTimer.Start();
-            
             _bc = new BackgroundWorkerControl(this.BackgroundWorkerCompleted, Environment.ExpandEnvironmentVariables("%BIOSANDBOX_HOME%"), _parent.textBox1);
-            _bc.AsyncTrening();
-        }
-
-        private void dispatcherTimer_Tick(object sender, EventArgs e)
-        {
-            
-            int i = Int32.Parse(label2.Content.ToString());
-            if(i==0)
-                Thread.Sleep(1000);
-            i++;
-            label2.Content = i;
-
-            // vyber akychsi strednych casov, obrazky, ktorych cas vytvorenia sa bude blizit k nim sa odoslu na DB
-            if (i == 5)
-                _times[0] = DateTime.Now;
-            if (i == 9)
-                _times[1] = DateTime.Now;
-            if (i == 13)
-                _times[2] = DateTime.Now;
-
-            if (i < 7)
-                label1.Content = "Pozeraj sa rovno";
-            else if (i < 11)
-                label1.Content = "Natoč sa doľava";
-            else if (i < 15)
-                label1.Content = "Natoč sa doprava";
-            else
-            {
-                this.EndBiosandboxProc();
-                label1.Content = "Snímky sa odosielajú do databázy";
-                _bc.AsyncTreningUpload(_times, _personName);
-            }
-        }
-
-        private void EndBiosandboxProc()
-        {
-            _bc.BeginTreningEnd();
-            _dispatcherTimer.Stop();
+            _bc.AsyncTest();
         }
 
         private void EndProcess()
         {
-            _bc.BeginTreningUploadEnd();
+            _bc.BeginTestEnd();
             _parent.EndAsyncOperation();
-            _dispatcherTimer.Stop();
             this.Close();
         }
 
@@ -109,7 +57,7 @@ namespace FaceRecognitionClient
             if (e.Error != null)
             {
                 _parent.textBox1.Text += Tools.GetErrorMessage(e.Error.Message);
-                this.EndBiosandboxProc();
+                this.EndProcess();
             }
             else if (e.Cancelled) // sem by nikdy nemal vbehnut
             {
@@ -120,7 +68,7 @@ namespace FaceRecognitionClient
                 // flag may not have been set, even though 
                 // CancelAsync was called.
                 _parent.textBox1.Text += Tools.GetLogMessage("Canceled");
-                this.EndBiosandboxProc();
+                this.EndProcess();
             }
             else
             {
@@ -129,13 +77,26 @@ namespace FaceRecognitionClient
                 _parent.textBox1.Text += Tools.GetLogMessage(e.Result.ToString());
 
                 if (e.Result.ToString().StartsWith("Upload"))
-                    this.EndProcess();
+                    _bc.AsyncTestUpload();
             }
         }
 
         private void Window_Closed(object sender, EventArgs e)
         {
-            this.EndBiosandboxProc();
+            this.EndProcess();
         }
+
+        // start
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            _bc.AsyncTestUpload();
+        }
+
+        // cancel
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.EndProcess();
+        }
+
     }
 }
